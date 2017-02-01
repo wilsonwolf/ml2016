ed30de57900e320e438652e1bbf895a17b430698
Karl <karljiangster@gmail.com>
2017-02-01 11:45:36 -0500

    Gradient and Random Forest

--- assn2/movie_rev.py
+++ assn2/movie_rev.py
@@ -2,6 +2,7 @@ import pandas as pd
 from sklearn.model_selection import GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier, GradientBoostingClassifier
+import matplotlib.pyplot as plt
 
 movie_tr = pd.read_csv("MovieReview_train.csv") 
 X_tr = movie_tr.drop('sentiment', 1) 
@@ -16,14 +17,14 @@ for d in max_depths:
 	estimators.append( DecisionTreeClassifier(max_depth = d) ) 
 
 ada_params = {'base_estimator': estimators,
-	'n_estimators': list(range(50, 200, 10)), 
+	'n_estimators': list(range(200, 300, 10)), 
 	'learning_rate': [0.01]  
 	} 
-rfc_params = {'n_estimators': list(range(50, 200, 10)), 
-	'max_depth': list(range(1, 5))
+rfc_params = {'n_estimators': list(range(200, 300, 10)), 
+	'max_depth': list(range(2, 12, 2))
 	}
 
-gb_params = {'n_estimators': list(range(50, 200, 10)), 
+gb_params = {'n_estimators': list(range(200, 300, 10)), 
 	'max_depth': list(range(1,5)) 
 	} 
 
@@ -31,4 +32,13 @@ ada_clf = GridSearchCV( AdaBoostClassifier(), ada_params, scoring = "accuracy", 
 rfc_clf= GridSearchCV( RandomForestClassifier(), rfc_params, scoring = "accuracy", cv = 10) 
 gb_clf = GridSearchCV( GradientBoostingClassifier(), gb_params, scoring = "accuracy", cv = 10)
 
-ada_clf.fit(X_tr, y_tr) 
+#ada_clf.fit(X_tr, y_tr) 
+#pd.to_pickle(ada_clf, "ada_gridsearch.pkl") 
+
+rfc_clf.fit(X_tr, y_tr) 
+pd.to_pickle(rfc_clf, "rfc_gridsearch.pkl") 
+
+gb_clf.fit(X_tr, y_tr) 
+pd.to_pickle(gb_clf, "gb_gridsearch.pkl") 
+
+
